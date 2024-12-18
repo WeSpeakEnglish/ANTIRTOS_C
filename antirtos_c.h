3d73fa3311a7ef2e0d9aec568bc9bec70ed8dc77
Aleksei Tertychnyi <admin@pollutants.eu>
2024-12-18 16:13:02 +0100

    delayed functionality added to conveyers without parameters

--- antirtos_c.h
+++ antirtos_c.h
@@ -1,5 +1,3 @@
-//-8<--8<--8<--ANTIRTOS_C BEGIN--8<--8<--8<--8<---
-#ifndef fQ
 #define fQ(q, Q_SIZE)                            \
     volatile int q##_last = 0;                   \
     int q##_first = 0;                           \
@@ -16,12 +14,51 @@
             return 1; /* Queue is empty */       \
         q##_Queue[q##_first++]();                \
         q##_first %= Q_SIZE;                     \
-        return 0;                                \
+        return 0;  /* Success */ 				 \
     }
-#endif
+
+#define del_fQ(q, Q_SIZE)                                           \
+    int q##_time = 0;                                               \
+    void (*q##_del_fQueue[Q_SIZE])(void);                           \
+    int q##_execArr[Q_SIZE] = { 0 };   	                            \
+    int q##_execTime[Q_SIZE];                                       \
+    fQ(q, Q_SIZE)                                                   \
+    int q##_Push_delayed(void (*pointerF)(void), int delayTime){    \
+        int q##_fullQ = 1;                                          \
+        for (int i = 0; i < Q_SIZE; i++) {                          \
+            if (!q##_execArr[i]) {                                  \
+                q##_del_fQueue[i] = pointerF;                       \
+                q##_execArr[i] = 1;                                 \
+                q##_execTime[i] = q##_time + delayTime;             \
+                q##_fullQ = 0;                                      \
+                break;                                              \
+            }                                                       \
+        }                                                           \
+        return q##_fullQ;                                           \
+    }                                                               \
+    void q##_Tick(void){                                            \
+        for (int i = 0; i < Q_SIZE; i++) {                          \
+            if (q##_execTime[i] == q##_time) {                      \
+                if (q##_execArr[i]) {                               \
+                    q##_Push(q##_del_fQueue[i]);                    \
+                    q##_execArr[i] = 0;                             \
+                }                                                   \
+            }                                                       \
+        }                                                           \
+        q##_time++; /* Increment time */                            \
+    }                                                               \
+    int q##_Revoke(void (*pointerF)(void)){                         \
+        int result = 1;                                             \
+        for (int i = 0; i < Q_SIZE; i++) {                          \
+            if (q##_del_fQueue[i] == pointerF) {                    \
+                q##_execArr[i] = 0;                                 \
+                result = 0;                                         \
+            }                                                       \
+        }                                                           \
+        return result;                                              \
+    }
 	
-#ifndef fQP
-#define fQP(q, Q_SIZE, param_type)                                              \
+	#define fQP(q, Q_SIZE, param_type)                                          \
     void (*q##_funcs[Q_SIZE])(param_type);                                      \
     param_type q##_params[Q_SIZE];                                              \
     volatile int q##_last = 0;                                                  \
@@ -41,5 +78,5 @@
         q##_first %= Q_SIZE;                                                    \
         return 0; /* Success */                                                 \
     }
-#endif	
-//-8<--8<--8<--ANTIRTOS_C END --8<--8<--8<--8<---
+	
+	
